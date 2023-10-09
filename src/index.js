019d8ead6893c2a47db4e4ed5cc517ff9b5c6f83
Oleh Panchenko <106483317+opanchen@users.noreply.github.com>
2023-10-09 15:35:01 +0200

    updated index.js
    - inserted Provider-component and PersistGate for redux state into root index.js file;

--- src/index.js
+++ src/index.js
@@ -3,12 +3,19 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
+import { PersistGate } from "redux-persist/integration/react";
+import { persistor, store } from "redux/store";
+import { Provider } from "react-redux";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <BrowserRouter basename="/react-phonebook">
-      <App />
-    </BrowserRouter>
+    <Provider store={store}>
+      <PersistGate persistor={persistor}>
+        <BrowserRouter basename="/react-phonebook">
+          <App />
+        </BrowserRouter>
+      </PersistGate>
+    </Provider>
   </React.StrictMode>
 );
