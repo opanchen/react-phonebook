0976bf72542a910214f8ce345bbc5323f936fc35
Oleh Panchenko <106483317+opanchen@users.noreply.github.com>
2023-10-30 19:13:23 +0100

    updated Home.jsx

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,4 +1,7 @@
+import { Container } from "components";
 import { Helmet } from "react-helmet";
+import heroImg from "../../assets/images/app.jpg";
+import css from "./Home.module.css";
 
 const Home = () => {
   return (
@@ -7,7 +10,19 @@ const Home = () => {
         <title>Home</title>
       </Helmet>
 
-      <div>Home page</div>
+      <Container>
+        <section className={css.wrapper}>
+          <div className={css.text}>
+            <h1 className={css.heading}>Welcome to Phonebook Application!</h1>
+            <p className={css.desc}>
+              Create and manage your personal contact list, save favorite ones,
+              send them messages or just do everything you want.
+            </p>
+          </div>
+
+          <img src={heroImg} alt="application desktop" width={400} />
+        </section>
+      </Container>
     </>
   );
 };
